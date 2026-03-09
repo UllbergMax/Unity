5bda564329a187fab766da585d9d5874e4f11969
Ullbergm <ullbergm@arcada.fi>
2026-03-09 14:48:31 +0200

    Fix obstacle collision and inspector setup for Scene1

--- Assets/Scripts/Scene 1/GoalTrigger.cs
+++ Assets/Scripts/Scene 1/GoalTrigger.cs
@@ -3,12 +3,17 @@ using UnityEngine.SceneManagement;
 
 public class GoalTrigger : MonoBehaviour
 {
+    public GameObject bee;
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            BeeChaser.gameWon = true;
+            // stop the bee immediately
+            if (bee != null)
+                bee.SetActive(false);
 
+            // load next scene
             int i = SceneManager.GetActiveScene().buildIndex;
             SceneManager.LoadScene(i + 1);
         }
